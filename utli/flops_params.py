e1b84be4d146d3a2bc493b93e52dcca005b8db1a
starsky68 <31984917+starsky68@users.noreply.github.com>
2025-04-27 11:26:26 +0800

    Update flops_params.py

--- utli/flops_params.py
+++ utli/flops_params.py
@@ -2,7 +2,7 @@ import torch
 from thop import profile
 from model.DCRetinex import DCRetinex
 
-model = PINet()
+model = DCRetinex()
 input = torch.randn(1, 3, 128, 128) # 960，720
 flops, params = profile(model, inputs=(input, ))
 print('FLOPs = ' + str(flops/1000**3) + 'G')
