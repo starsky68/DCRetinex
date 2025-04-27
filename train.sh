a68cedb157b2d8c565f673f6817ac5552f229d75
starsky68 <31984917+starsky68@users.noreply.github.com>
2025-04-27 11:21:23 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
 #SICE 0.14
-#python3 train.py --model_name 'PairLIE' --loss_name 'PairLIELoss' --lc 0.2
-#python3 train.py --model_name 'PairLIE' --loss_name 'RePILoss' --lc 0.2
-#python3 train.py --model_name 'HgNet' --loss_name 'RePILoss' --lc 0.17
-#python3 train.py --model_name 'RePINet' --loss_name 'RePILoss' --lc 0.17
-#python3 train.py --model_name 'PINetv2' --loss_name 'RePILoss' --lc 0.17
-#python3 train.py --model_name 'HgNet' --loss_name 'PairLIELoss' --lc 0.17
-#python3 train.py --model_name 'RePINet' --loss_name 'PairLIELoss' --lc 0.17
-#python3 train.py --model_name 'PINetv2' --loss_name 'PairLIELoss' --lc 0.17
-
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossTP' --t_model_type 'zdce' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossT' --t_model_type 'zdce' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossP' --t_model_type 'zdce' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossTP' --t_model_type 'urtx' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossT' --t_model_type 'urtx' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossP' --t_model_type 'urtx' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossSingle' --lc 0.14
-
-# 这里换muti-exposed 数据集了
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossTP' --t_model_type 'urtx' --lc 0.14
-# python3 train.py --model_name 'RePINet' --loss_name 'RePILossTP' --t_model_type 'zdce' --lc 0.14
-
-#python3 train.py --model_name 'RePINetv2' --loss_name 'RePILossT' --t_model_type 'zdce' --lc 0.17
-#LSRW
-# python3 train.py --model_name 'PINet' --loss_name 'PILIELoss' --lc 0.17
-#python3 train.py --model_name 'PINet' --loss_name 'PILIELoss' --lc 0.16
-#python3 train.py --model_name 'PINet' --loss_name 'PILIELoss' --lc 0.19
+# python3 train.py --model_name 'DCRetinex' --loss_name 'TMGLoss' --t_model_type 'zdce' --lc 0.14
+# python3 train.py --model_name 'DCRetinex' --loss_name 'PUEILoss' --t_model_type 'zdce' --lc 0.14
+# python3 train.py --model_name 'DCRetinex' --loss_name 'TMGLoss' --t_model_type 'urtx' --lc 0.14
+# python3 train.py --model_name 'DCRetinex' --loss_name 'PUEILoss' --t_model_type 'urtx' --lc 0.14
+# python3 train.py --model_name 'DCRetinex' --loss_name 'SUEILoss' --lc 0.14
